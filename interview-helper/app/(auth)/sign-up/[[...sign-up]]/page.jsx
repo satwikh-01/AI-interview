bff35f6b11005a2681d05488ad2fe1ce3120a250
Satwik Hegde <satwikh_01@icloud.com>
2024-07-24 03:14:23 +0530

    Need to fix microphone

--- interview-helper/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ interview-helper/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,17 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex-1 flex items-center justify-center h-screen">
+    <div className="w-full max-w-md space-y-8 px-4 bg-white text-gray-600 sm:px-0">
+        
+        <div className="grid grid-cols-3 gap-x-3">
+            
+        </div>
+    
+        <SignUp />
+    </div>
+</div>
+
+  )
 }
--- interview-helper/app/dashboard/_components/Header.jsx
+++ interview-helper/app/dashboard/_components/Header.jsx
@@ -6,43 +6,21 @@ import { usePathname } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 function Header() {
+    const path = usePathname();
 
-    const path=usePathname();
-    useEffect(()=>{
+    useEffect(() => {
         console.log(path)
-    },[])
+    }, [])
 
-  return (
-    <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-        <Image src={'./Logo.svg'} width={160} height={100} alt='logo' />
-        <ul className='hidden md:flex gap-6'>
-          <Link href={"/dashboard"}>
-            <li className={`hover:text-primary hover:font-bold transition-all
-            cursor-pointer
-            ${path=='/dashboard'&&'text-primary font-bold'}
-            `}
-            
-            >Dashboard</li>
-            </Link>
-            
-            <li className={`hover:text-primary hover:font-bold transition-all
-            cursor-pointer
-            ${path=='/dashboard/questions'&&'text-primary font-bold'}
-            `}>Questions</li>
-              <Link href={"/dashboard/upgrade"}>
-            <li className={`hover:text-primary hover:font-bold transition-all
-            cursor-pointer
-            ${path=='/dashboard/upgrade'&&'text-primary font-bold'}
-            `}>Upgrade</li>
-            </Link>
-            <li className={`hover:text-primary hover:font-bold transition-all
-            cursor-pointer
-            ${path=='/dashboard/how'&&'text-primary font-bold'}
-            `}>How it Works?</li>
-        </ul>
-        <UserButton/>
-    </div>
-  )
+    return (
+        <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
+            <div className="flex items-center">
+                <Image src='/Logo.svg' width={160} height={100} alt='logo' />
+                <h1 style={{ fontSize: '2rem', fontWeight: 'bold', marginLeft: '10px' }}>InterviewAce</h1>
+            </div>
+            <UserButton />
+        </div>
+    )
 }
 
 export default Header

255898c860a4334f645433b13bcff2cddc2f267e
Satwik Hegde <satwikh_01@Satwiks-MacBook-Air.local>
2024-07-24 11:18:46 +0530

    Microphone fixed and ready to deploy

--- interview-helper/app/page.js
+++ interview-helper/app/page.js
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Image from "/Logo.svg";
 import Header from "./dashboard/_components/Header";
 import { AtomIcon, Edit, Share2 } from "lucide-react";
 
